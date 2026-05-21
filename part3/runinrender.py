581b47354f5f92d029f83a60e110ab90974cf017
LAJS1022 <luisssajs@gmail.com>
2026-05-20 23:17:37 -0600

    Update runinrender.py

--- part3/runinrender.py
+++ part3/runinrender.py
@@ -4,11 +4,12 @@ from app import create_app, db
 config_name = os.getenv('FLASK_ENV', 'development')
 app = create_app(config_name)
 
+print("Current directory:", os.getcwd())
+print("Files here:", os.listdir('.'))
+
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    print("Current directory:", os.getcwd())
-    print("Files here:", os.listdir('.'))
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
