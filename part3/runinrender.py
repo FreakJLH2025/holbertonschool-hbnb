b1398fe9c54e87916a2d8bfc761047e7be92a651
LAJS1022 <luisssajs@gmail.com>
2026-05-20 23:13:55 -0600

    Update runinrender.py

--- part3/runinrender.py
+++ part3/runinrender.py
@@ -8,5 +8,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
+    print("Current directory:", os.getcwd())
+    print("Files here:", os.listdir('.'))
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
